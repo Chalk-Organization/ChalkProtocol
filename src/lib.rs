dbea96010921a222c183d2c127a5475bf0e5970d
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-16 17:27:21 -0400

    Added IP

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-pub struct UDPClient {}
+use std::net::SocketAddrV4;
+
+pub struct UDPClient {
+    address: SocketAddrV4,
+}
 
 impl UDPClient {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new<T: Into<SocketAddrV4>>(ip: T) -> Self {
+        Self { address: ip.into() }
     }
 }
