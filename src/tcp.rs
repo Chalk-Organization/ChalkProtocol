01a801a362745e1669f0ea22a3d4bdbf0c498dc9
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-17 10:26:34 -0400

    Added Doc Comment

--- src/tcp.rs
+++ src/tcp.rs
@@ -9,6 +9,13 @@ impl TCPClient {
         Self { streams: vec![] }
     }
 
+    /// Connects to an IP Address given.
+    ///
+    /// # Example
+    /// ```
+    /// let mut client = TCPClient::new();
+    /// cilent.connect_to("127.0.0.1:8080");
+    /// ```
     pub fn connect_to<T: ToSocketAddrs>(&mut self, addr: T) -> &mut Self {
         self.streams
             .push(TcpStream::connect(addr).expect("Failed to Connect to IP Address"));
