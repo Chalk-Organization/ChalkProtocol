2e4bb81aabe885de9cde031e0452435976107e05
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-17 14:39:48 -0400

    updated test to include read/write

--- tests/tcp_client.rs
+++ tests/tcp_client.rs
@@ -2,5 +2,13 @@ use chalk_protocol::tcp::TCPClient;
 
 fn main() {
     let mut client = TCPClient::new();
-    client.connect_to("127.0.0.1:8080").unwrap();
+    let data = &mut [0; 3];
+    client
+        .connect_to("127.0.0.1:8080")
+        .unwrap()
+        .write(0, &[0, 1, 2])
+        .unwrap()
+        .read(0, data)
+        .unwrap();
+    assert_eq!(data, &mut [0, 1, 2]);
 }
