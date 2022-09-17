058db3bacac3ed3e9a3b943b1a87b24535881000
Lemon <55360995+LemonjamesD@users.noreply.github.com>
2022-09-17 09:49:51 -0400

    Removed Code because of #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,1 @@
-use std::net::SocketAddrV4;
 
-pub struct UDPClient {
-    address: SocketAddrV4,
-}
-
-impl UDPClient {
-    pub fn new<T: Into<SocketAddrV4>>(ip: T) -> Self {
-        Self { address: ip.into() }
-    }
-}
