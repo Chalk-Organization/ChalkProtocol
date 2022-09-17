722590464d0bbcb1b5a0b8241de0e560f8d864f6
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-17 10:21:40 -0400

    Added connecting to an IP Address

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,7 +1,17 @@
-pub struct TCPClient {}
+use std::net::{TcpStream, ToSocketAddrs};
+
+pub struct TCPClient {
+    streams: Vec<TcpStream>,
+}
 
 impl TCPClient {
     pub fn new() -> Self {
-        Self {}
+        Self { streams: vec![] }
+    }
+
+    pub fn connect_to<T: ToSocketAddrs>(&mut self, addr: T) -> &mut Self {
+        self.streams
+            .push(TcpStream::connect(addr).expect("Failed to Connect to IP Address"));
+        self
     }
 }
