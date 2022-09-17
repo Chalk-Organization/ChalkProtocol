ff4027843ea6b9b19a0ecec4d8b3726a9603e09d
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-17 13:00:29 -0400

    Added `connect_to_inner`

--- src/tcp.rs
+++ src/tcp.rs
@@ -9,16 +9,8 @@ impl TCPClient {
         Self { streams: vec![] }
     }
 
-    /// Connects to an IP Address given.
-    ///
-    /// # Example
-    /// ```
-    /// use chalk_protocol::tcp::TCPClient;
-    ///
-    /// let mut client = TCPClient::new();
-    /// client.connect_to("127.0.0.1:8080").unwrap();
-    /// ```
-    pub fn connect_to<T: ToSocketAddrs>(&mut self, addr: T) -> Result<&mut Self, String> {
+    // Inner version of `connect_to` to prevent code duplication
+    fn connect_to_inner<T: ToSocketAddrs>(&mut self, addr: T) -> Result<&mut Self, String> {
         self.streams.push(
             TcpStream::connect(addr)
                 // Checks if there is an error an if so then map the error to a custom error
@@ -34,15 +26,24 @@ impl TCPClient {
     /// use chalk_protocol::tcp::TCPClient;
     ///
     /// let mut client = TCPClient::new();
-    /// client.connect_to_unchecked("127.0.0.1:8080");
+    /// client.connect_to("127.0.0.1:34254").unwrap();
+    /// ```
+    pub fn connect_to<T: ToSocketAddrs>(&mut self, addr: T) -> Result<&mut Self, String> {
+        self.connect_to_inner(addr)
+    }
+
+    /// Connects to an IP Address given.
+    ///
+    /// Panics if can't connect to IP Address.
+    ///
+    /// # Example
+    /// ```
+    /// use chalk_protocol::tcp::TCPClient;
+    ///
+    /// let mut client = TCPClient::new();
+    /// client.connect_to_unchecked("127.0.0.1:34254");
     /// ```
     pub fn connect_to_unchecked<T: ToSocketAddrs>(&mut self, addr: T) -> &mut Self {
-        self.streams.push(
-            TcpStream::connect(addr)
-                // Checks if there is an error an if so then map the error to a custom error
-                .map_err(|x| -> String { format!("Failed to Connect to IP Address: `{}`", x) })
-                .unwrap(),
-        );
-        self
+        self.connect_to_inner(addr).unwrap()
     }
 }
