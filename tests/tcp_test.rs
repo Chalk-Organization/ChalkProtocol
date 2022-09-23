3fa697118bbb9d8f84af8bb6e870bb894753b0d0
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-22 21:24:29 -0400

    Changed port

--- tests/tcp_test.rs
+++ tests/tcp_test.rs
@@ -4,8 +4,8 @@ use chalk_protocol::tcp::TcpClient;
 #[tokio::test]
 pub async fn tcp_test() -> Result<()> {
 	let client = TcpClient::new();
-	client.bind_to("127.0.0.1:8080").await?;
-	client.connect_to("127.0.0.1:8080").await?;
+	client.bind_to("127.0.0.1:36918").await?;
+	client.connect_to("127.0.0.1:36918").await?;
 
 	client.write(&[1; 4]).await?;
 	let mut data = [0; 4];
